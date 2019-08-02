a1cb71fb5aaca91d75b9cfaaac526b7e9ccffac6
DrCaticorn <51471043+DrCaticorn@users.noreply.github.com>
2019-08-02 02:13:23 +0000

    Initial commit

---
+++ cpp_session2/src/homework/Source.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+	Please feel free to add more functions to your
+	code if you would like to test out more of the
+	questions.  Make sure to keep the new functions
+	above the main() function and then to reference
+	them back in the main function.  It is also
+	recommended to comment out functions you are
+	not using to more accurately test out your
+	code.
+*/
+
+void question4() {
+
+}
+
+void question5() {
+
+}
+
+void question10() {
+
+}
+
+int main() {
+	question4();
+	question5();
+	question10();
+}
